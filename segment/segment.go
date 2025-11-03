0f04ba0b644807e9d8246f53cc42b4b3975a5c5e
Pranav Madhu <pranavmadhu2003@gmail.com>
2025-11-03 19:53:58 +0530

    Implements goroutines in the segment to achieve concurrency

--- segment/segment.go
+++ segment/segment.go
@@ -6,6 +6,7 @@ import (
 	"parser/model"
 	"parser/parser"
 	"path/filepath"
+	"sync"
 	"time"
 )
 
@@ -28,7 +29,11 @@ func SetSegmentIndex(entries []model.LogEntry) model.SegmentIndex {
 }
 
 func CreateSegments(path string) ([]model.Segment, error) {
+	start := time.Now()
 	var Segments []model.Segment
+
+	var wg sync.WaitGroup
+	var mu sync.Mutex
 
 	//enter folder
 	files, err := os.ReadDir(path)
@@ -42,29 +47,44 @@ func CreateSegments(path string) ([]model.Segment, error) {
 			continue
 		}
 
-		var segment model.Segment
-		segment.FileName = file.Name()
-		path := filepath.Join(path, file.Name())
-		allEntries, err := parser.ParseLogFile(path)
-		if err != nil {
-			fmt.Printf("Skipping file %s due to error: %v\n", path, err)
-			continue
+		wg.Add(1)
 
-		}
+		go func(file os.DirEntry) {
+			defer wg.Done()
 
-		segment.LogEntries = allEntries
+			var segment model.Segment
+			segment.FileName = file.Name()
+			filPath := filepath.Join(path, file.Name())
+			allEntries, err := parser.ParseLogFile(filPath)
+			if err != nil {
+				fmt.Printf("Skipping file %s due to error: %v\n", path, err)
+				return
 
-		for _, entry := range allEntries {
-			if segment.StartTime.Equal(time.Time{}) || entry.Time.Before(segment.StartTime) {
-				segment.StartTime = entry.Time
 			}
 
-			if entry.Time.After(segment.EndTime) {
-				segment.EndTime = entry.Time
+			segment.LogEntries = allEntries
+
+			for _, entry := range allEntries {
+				if segment.StartTime.Equal(time.Time{}) || entry.Time.Before(segment.StartTime) {
+					segment.StartTime = entry.Time
+				}
+
+				if entry.Time.After(segment.EndTime) {
+					segment.EndTime = entry.Time
+				}
 			}
-		}
-		segment.Index = SetSegmentIndex(allEntries)
-		Segments = append(Segments, segment)
+			segment.Index = SetSegmentIndex(allEntries)
+
+			// lock before writing to shared slice
+			mu.Lock()
+			Segments = append(Segments, segment)
+			mu.Unlock()
+
+		}(file)
 	}
+
+	wg.Wait()
+	elapsed := time.Since(start)
+	fmt.Println("Segmenting duration:", elapsed)
 	return Segments, nil
 }
