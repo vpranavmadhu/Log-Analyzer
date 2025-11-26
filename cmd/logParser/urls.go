7d312a13a5d682a14e90941e8a91b32374a276fb
Pranav Madhu <pranavmadhu2003@gmail.com>
2025-11-26 09:56:44 +0530

    Implements api for filtered entries with server-side pagination

--- cmd/logParser/urls.go
+++ cmd/logParser/urls.go
@@ -11,10 +11,30 @@ func SetupRoutes(db *gorm.DB) *gin.Engine {
 	DB = db
 
 	r := gin.Default()
+
+	r.Use(CORSMiddleware())
 	r.LoadHTMLGlob("templates/*.html")
 
-	r.GET("/", showAllLogs) //first time loading
-	r.POST("/", filterLogs) // after searching
+	r.GET("/", showAllLogsHTML) //for html
+	r.GET("/logs", showAllLogs) //first time loading
+	r.GET("/alllogs", showAllLogsPaginated)
+	r.POST("/filteredlogs", filterLogs)   // after searching
+	r.POST("/filterlogs", filterLogsJSON) //react filter
+	r.POST("/filter", filterLogsPaginated)
 
 	return r
+}
+
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.Header("Access-Control-Allow-Origin", "*")
+		c.Header("Access-Control-Allow-Credentials", "true")
+		c.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Header("Access-Control-Allow-Methods", "POST,HEAD,PATCH, OPTIONS, GET, PUT")
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+		c.Next()
+	}
 }
--- cmd/logParser/views.go
+++ cmd/logParser/views.go
@@ -2,13 +2,62 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	models "parser/pkg/dbmodels"
+	"strconv"
 	"strings"
 
 	"github.com/gin-gonic/gin"
 )
 
+// logs/
 func showAllLogs(c *gin.Context) {
+	entries, err := models.Query(DB, []string{}) //empty filter to get all logs
+	if err != nil {
+		c.JSON(500, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(200, gin.H{
+		"entries": entries,
+		"count":   len(entries),
+	})
+}
+
+func showAllLogsPaginated(c *gin.Context) {
+	page, _ := strconv.Atoi(c.DefaultQuery("page", "0"))
+	pageSize, _ := strconv.Atoi(c.DefaultQuery("pageSize", "100"))
+
+	offset := page * pageSize
+
+	var entries []models.Entry
+	var count int64
+
+	DB.Model(&models.Entry{}).Count(&count)
+	err := DB.
+		Model(&models.Entry{}).
+		Preload("Level").
+		Preload("Component").
+		Preload("Host").
+		Order("id ASC").
+		Offset(offset).
+		Limit(pageSize).
+		Find(&entries).Error
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"entries": entries,
+		"count":   count,
+	})
+	fmt.Println("page:", page, "pageSize:", pageSize, "offset:", offset)
+
+}
+
+func showAllLogsHTML(c *gin.Context) {
 	entries, err := models.Query(DB, []string{}) //empty filter to get all logs
 	if err != nil {
 		c.HTML(500, "result.html", gin.H{"error": err.Error()})
@@ -21,6 +70,7 @@ func showAllLogs(c *gin.Context) {
 	})
 }
 
+// filteredlogs/
 func filterLogs(c *gin.Context) {
 	queryParts := []string{}
 
@@ -61,4 +111,87 @@ func filterLogs(c *gin.Context) {
 		"entries": entries,
 		"count":   len(entries),
 	})
+}
+
+func filterLogsJSON(c *gin.Context) {
+	var body map[string][]string
+
+	if err := c.ShouldBindJSON(&body); err != nil {
+		c.JSON(400, gin.H{
+			"error": "Invalid JSON body",
+		})
+		return
+	}
+
+	queryParts := []string{}
+
+	for key, vals := range body {
+		if len(vals) > 0 {
+			joined := strings.Join(vals, ",")
+			queryParts = append(queryParts, fmt.Sprintf("%s=%s", key, joined))
+		}
+	}
+
+	entries, err := models.Query(DB, queryParts)
+	if err != nil {
+		c.JSON(500, err)
+		return
+	}
+
+	c.JSON(200, gin.H{
+		"entries": entries,
+		"count":   len(entries),
+	})
+}
+
+func filterLogsPaginated(c *gin.Context) {
+
+	var body map[string][]string
+
+	if err := c.ShouldBindJSON(&body); err != nil {
+		c.JSON(400, gin.H{
+			"error": "Invalid JSON body",
+		})
+		return
+	}
+
+	page, _ := strconv.Atoi(c.DefaultQuery("page", "0"))
+	pageSize, _ := strconv.Atoi(c.DefaultQuery("pageSize", "100"))
+
+	offset := page * pageSize
+
+	queryParts := []string{}
+
+	for key, vals := range body {
+		if len(vals) > 0 {
+			joined := strings.Join(vals, ",")
+			queryParts = append(queryParts, fmt.Sprintf("%s=%s", key, joined))
+		}
+	}
+
+	filteredEntries, err := models.Query(DB, queryParts)
+	if err != nil {
+		c.JSON(500, err)
+		return
+	}
+
+	total := len(filteredEntries)
+	// Apply manual pagination to filtered data
+	start := offset
+	end := offset + pageSize
+
+	if start > total {
+		start = total
+	}
+	if end > total {
+		end = total
+	}
+
+	pageEntries := filteredEntries[start:end]
+
+	c.JSON(http.StatusOK, gin.H{
+		"entries": pageEntries,
+		"count":   total,
+	})
+
 }
